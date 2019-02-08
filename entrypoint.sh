b44b371a86dfbef5b6bc6fa1368ab382e0434a1a
wojiushixiaobai <296015668@qq.com>
2019-02-08 11:26:07 +0800

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [ ! -f "/opt/coco/config.yml" ]; then
     cp /opt/coco/config_example.yml /opt/coco/config.yml
     sed -i '5d' /opt/coco/config.yml
     sed -i "5i CORE_HOST: $CORE_HOST" /opt/coco/config.yml
-    sed -i "s/BOOTSTRAP_TOKEN: <ChangeIT>/BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN/g" /opt/coco/config.yml
+    sed -i "s/BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>/BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN/g" /opt/coco/config.yml
     sed -i "s/# LOG_LEVEL: INFO/LOG_LEVEL: ERROR/g" /opt/coco/config.yml
 fi
 
